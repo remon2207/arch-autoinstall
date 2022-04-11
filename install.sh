2d4d8206ef245c37717f21fd4c28c7b31950d786
remon2207 <39646984+remon2207@users.noreply.github.com>
2022-04-11 11:23:01 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -80,52 +80,52 @@ arch-chroot /mnt sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' -e 's/#ja_
 arch-chroot /mnt locale-gen
 echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
 echo KEYMAP=jp106 > /mnt/etc/vconsole.conf
-echo $4 > /mnt/etc/hostname
+echo $5 > /mnt/etc/hostname
 # arch-chroot /mnt sh -c "echo '%wheel ALL=(ALL) ALL' | EDITOR='tee -a' visudo"
 
 ip_address=$(ip -4 a show enp6s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-echo -e "127.0.0.1       localhost\n::1             localhost\n${ip_address}    $4.localdomain        $4" >> /mnt/etc/hosts
+echo -e "127.0.0.1       localhost\n::1             localhost\n${ip_address}    $5.localdomain        $5" >> /mnt/etc/hosts
 arch-chroot /mnt cp /etc/netctl/examples/ethernet-static /etc/netctl/enp6s0
 dns="'8.8.8.8' '8.8.4.4'"
 google_dns="$dns"
 arch-chroot /mnt sed -i -e "/^Interface/s/eth0/enp6s0/" -e "/^Address/c\Address=('${ip_address}/24')" -e "/^DNS/c\DNS=(${google_dns})" /etc/netctl/enp6s0
 arch-chroot /mnt netctl enable enp6s0
 
-arch-chroot /mnt sh -c "echo '%wheel ALL=(ALL) ALL' | EDITOR='tee -a' visudo"
+echo '%wheel ALL=(ALL) ALL' | arch-chroot /mnt EDITOR='tee -a' visudo"
 # sed -i -e "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /mnt/etc/sudoers
 echo ------------------------------------------------------------------
 echo "Password for root"
 # arch-chroot /mnt passwd
-echo "root:$7" | chpasswd
-arch-chroot /mnt useradd -m -g users -G wheel -s /bin/bash $5
+echo "root:$8" | chpasswd
+arch-chroot /mnt useradd -m -g users -G wheel -s /bin/bash $6
 
-# echo "Password for $5"
-# arch-chroot /mnt passwd $5
-echo "$5:$6" | arch-chroot /mnt chpasswd
+# echo "Password for $6"
+# arch-chroot /mnt passwd $6
+echo "$6:$7" | arch-chroot /mnt chpasswd
 
 # arch-chroot /mnt sudo -u $5 mkdir /home/$5/appimage
 # arch-chroot /mnt sudo -u $5 wget -O /home/$5/appimage/nvim.appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 # arch-chroot /mnt sudo -u $5 chmod u+x /home/$5/appimage/nvim.appimage
 
-echo -e "clear lock\nclear control\nkeycode 66 = Control_L\nadd control = Control_L Control_R" > /mnt/home/$5/.Xmodmap
-arch-chroot /mnt chown $5:users /home/$5/.Xmodmap
-arch-chroot /mnt chmod 644 /home/$5/.Xmodmap
-arch-chroot /mnt sudo -u $5 xmodmap /home/$5/.Xmodmap
+# echo -e "clear lock\nclear control\nkeycode 66 = Control_L\nadd control = Control_L Control_R" > /mnt/home/$6/.Xmodmap
+# arch-chroot /mnt chown $5:users /home/$5/.Xmodmap
+# arch-chroot /mnt chmod 644 /home/$5/.Xmodmap
+# arch-chroot /mnt sudo -u $5 xmodmap /home/$5/.Xmodmap
 
 # echo -e "export GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport XMODIFIERS=@im=fcitx" > /mnt/home/$5/.xprofile
 echo -e "GTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx" >> /mnt/etc/environment
 # arch-chroot /mnt chown $5:users /home/$5/.xprofile
 # arch-chroot /mnt chmod 644 /home/$5/.xprofile
-arch-chroot /mnt mkdir /home/$5/git
-arch-chroot /mnt chown $5:users /home/$5/git
-arch-chroot /mnt chmod 755 /home/$5/git
+arch-chroot /mnt mkdir /home/$6/git
+arch-chroot /mnt chown $6:users /home/$6/git
+arch-chroot /mnt chmod 755 /home/$6/git
 # arch-chroot /mnt sed -i -e 's/en_US.UTF-8 UTF-8/#en_US.UTF-8 UTF-8/g' /etc/locale.gen
 # arch-chroot /mnt locale-gen
 echo LANG=ja_JP.UTF-8 > /mnt/etc/locale.conf
-git clone https://github.com/remon2207/dotfiles.git /mnt/home/$5/git/dotfiles
-arch-chroot /mnt chown -R $5:users /home/$5/git/dotfiles
-arch-chroot /mnt sudo -u $5 ./home/$5/git/dotfiles/arch_setup.sh
-arch-chroot /mnt sudo -u $5 ./home/$5/git/dotfiles/install.sh
+git clone https://github.com/remon2207/dotfiles.git /mnt/home/$6/git/dotfiles
+arch-chroot /mnt chown -R $6:users /home/$6/git/dotfiles
+arch-chroot /mnt sudo -u $6 ./home/$6/git/dotfiles/arch_setup.sh
+arch-chroot /mnt sudo -u $6 ./home/$6/git/dotfiles/install.sh
 
 arch-chroot /mnt cp -r /usr/share/pipewire /etc/pipewire
 
@@ -141,10 +141,10 @@ elif [ $3 = "kde" ] ; then
     arch-chroot /mnt systemctl enable lightdm
 elif [ $3 = "i3" ] ; then
     arch-chroot /mnt systemctl enable lightdm
-    git clone --depth=1 https://github.com/adi1090x/polybar-themes.git /mnt/home/$5/git/polybar-themes
-    arch-chroot /mnt chown -R $5:users /home/$5/git/polybar-themes
-    arch-chroot /mnt chmod +x /home/$5/git/polybar-themes/setup.sh
-    arch-chroot /mnt sudo -u $5 ./home/$5/git/polybar-themes/setup.sh
+    git clone --depth=1 https://github.com/adi1090x/polybar-themes.git /mnt/home/$6/git/polybar-themes
+    arch-chroot /mnt chown -R $6:users /home/$6/git/polybar-themes
+    arch-chroot /mnt chmod +x /home/$6/git/polybar-themes/setup.sh
+    arch-chroot /mnt sudo -u $6 ./home/$6/git/polybar-themes/setup.sh
 fi
 
 # arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
