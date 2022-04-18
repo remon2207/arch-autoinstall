a6215e07e6787ab9b9908cd164704f8242f8e967
remon2207 <39646984+remon2207@users.noreply.github.com>
2022-04-18 16:54:14 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -153,24 +153,23 @@ fi
 #arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 # systemd-boot
-root_partuuid=`blkid -s PARTUUID -o value ${1}2`
-
 arch-chroot /mnt bootctl --path=/boot install
 echo -e "default    arch\ntimeout    10\nconsole-mode max\neditor     no" >> /mnt/boot/loader/loader.conf
+root_partuuid=`blkid -s PARTUUID -o value ${1}2`
 echo -e "title    Arch Linux\nlinux    /vmlinuz-linux-zen\ninitrd   /intel-ucode.img\ninitrd   /initramfs-linux-zen.img\noptions  root=PARTUUID=${root_partuuid} rw loglevel=3 nomodeset i915.modeset=0 nouveau.modeset=0 nvidia-drm.modeset=1" >> /mnt/boot/loader/entries/arch.conf
-arch-chroot /mnt systemctl enable --now systemd-boot-update.service
 
 efi_uuid=`blkid -s UUID -o value ${1}1`
 root_uuid=`blkid -s UUID -o value ${1}2`
 home_uuid=`blkid -s UUID -o value ${1}3`
 
 efi_partuuid=`blkid -s PARTUUID -o value ${1}1`
-root_partuuid=`blkid -s PARTUUID -o value ${1}2`
 home_partuuid=`blkid -s PARTUUID -o value ${1}3`
 
 arch-chroot /mnt sed -i -e "s/UUID=${efi_uuid}/PARTUUID=${efi_partuuid}/" /etc/fstab
 arch-chroot /mnt sed -i -e "s/UUID=${root_uuid}/PARTUUID=${root_partuuid}/" /etc/fstab
 arch-chroot /mnt sed -i -e "s/UUID=${home_uuid}/PARTUUID=${home_partuuid}/" /etc/fstab
+
+arch-chroot /mnt systemctl enable --now systemd-boot-update.service
 
 umount -R /mnt
 systemctl reboot
