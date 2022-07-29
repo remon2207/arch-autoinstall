725f2f08711e65bfa226bd7ece26546579a89b32
remon2207 <reimond2207@gmail.com>
2022-07-29 13:59:44 +0900

    Update 2022/07/29 13:59:44

--- install.sh
+++ install.sh
@@ -213,10 +213,10 @@ arch-chroot /mnt sed -i "s/^#NTP=/NTP=0.asia.pool.ntp.org 1.asia.pool.ntp.org 2.
 arch-chroot /mnt sed -i "s/^#FallbackNTP/FallbackNTP/" /etc/systemd/timesyncd.conf
 
 arch-chroot /mnt sed -i "s/-march=x86-64 -mtune=generic/-march=native/" /etc/makepkg.conf
-arch-chroot /mnt sed -i 's/^#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(($(nproc)+1))"' /etc/makepkg.conf
+arch-chroot /mnt sed -i 's/^#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(($(nproc)+1))"/' /etc/makepkg.conf
 arch-chroot /mnt sed -i "s/^#BUILDDIR/BUILDDIR/" /etc/makepkg.conf
-arch-chroot /mnt sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z --threads=0 -)" /etc/makepkg.conf
-arch-chroot /mnt sed -i "^#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=10s" /etc/systemd/system.conf
+arch-chroot /mnt sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z --threads=0 -)/" /etc/makepkg.conf
+arch-chroot /mnt sed -i "^#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=10s/" /etc/systemd/system.conf
 
 if [ ${3} = "xfce" ] ; then
     arch-chroot /mnt systemctl enable lightdm
