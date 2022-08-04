4c647214ae1288414238e524bbb4746c3141ebf8
remon2207 <reimond2207@gmail.com>
2022-08-04 16:20:55 +0900

    Update 2022/08/04 16:20:55

--- install.sh
+++ install.sh
@@ -63,9 +63,9 @@ if [ ${#} -lt 11 ]; then
     echo "Usage:"
     echo "install.sh \
         <disk> <microcode: intel | amd> <DE: xfce | gnome | kde> \
-        <GPU: nvidia | amd | intel> <HostName> <UserName> \
-        <userPasword> <rootPassword> <partition-table-destroy: yes | no-exclude-efi | no-root-only | skip> \
-        <boot-loader: systemd-boot | grub> <network: static-ip | dhcp>"
+<GPU: nvidia | amd | intel> <HostName> <UserName> \
+<userPasword> <rootPassword> <partition-table-destroy: yes | no-exclude-efi | no-root-only | skip> \
+<boot-loader: systemd-boot | grub> <network: static-ip | dhcp>"
     exit
 fi
 
