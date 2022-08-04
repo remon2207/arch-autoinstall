cbd2bf1041c789115d5dcfee63bc1125599fb42b
remon2207 <reimond2207@gmail.com>
2022-08-04 16:13:19 +0900

    Update 2022/08/04 16:13:19

--- install.sh
+++ install.sh
@@ -93,14 +93,14 @@ check_variables() {
 
 selection_arguments() {
     # intel-ucode or amd-ucode
-    if [ ${2} = "intel" ] ; then
+    if [ ${2} = "intel" ]; then
         packagelist="${packagelist} intel-ucode"
-    elif [ ${2} = "amd" ] ; then
+    elif [ ${2} = "amd" ]; then
         packagelist="${packagelist} amd-ucode"
     fi
 
     # desktop
-    if [ ${3} = "xfce" ] ; then
+    if [ ${3} = "xfce" ]; then
         packagelist="${packagelist} \
             xfce4 \
             xfce4-goodies \
@@ -112,7 +112,7 @@ selection_arguments() {
             lxappearance-gtk3 \
             evince \
             xarchiver"
-    elif [ ${3} = "gnome" ] ; then
+    elif [ ${3} = "gnome" ]; then
         packagelist="${packagelist} \
             gdm \
             gnome-control-center \
@@ -129,7 +129,7 @@ selection_arguments() {
             file-roller \
             evince \
             gnome-backgrounds"
-    elif [ ${3} = "kde" ] ; then
+    elif [ ${3} = "kde" ]; then
         packagelist="${packagelist} \
             plasma-meta \
             lxappearance-gtk3 \
@@ -138,11 +138,11 @@ selection_arguments() {
             lightdm-gtk-greeter-settings"
     fi
 
-    if [ ${4} = "nvidia" ] ; then
+    if [ ${4} = "nvidia" ]; then
         packagelist="${packagelist} nvidia-dkms nvidia-settings"
-    elif [ ${4} = "amd" ] ; then
+    elif [ ${4} = "amd" ]; then
         packagelist="${packagelist} xf86-video-amdgpu libva-mesa-driver mesa-vdpau"
-    elif [ ${4} = "intel" ] ; then
+    elif [ ${4} = "intel" ]; then
         echo "Already declared."
     fi
 
@@ -156,7 +156,7 @@ time_setting() {
 }
 
 partitioning() {
-    if [ ${9} = "yes"]; then
+    if [ ${9} = "yes" ]; then
         sgdisk -Z ${1}
         sgdisk -n 0::+512M -t 0:ef00 -c 0:"EFI System" ${1}
         sgdisk -n 0::+350G -t 0:8300 -c 0:"Linux filesystem" ${1}
@@ -166,7 +166,7 @@ partitioning() {
         mkfs.fat -F 32 ${1}1
         mkfs.ext4 ${1}2
         mkfs.ext4 ${1}3
-    elif [ ${9} = "no-exclude-efi"]; then
+    elif [ ${9} = "no-exclude-efi" ]; then
         sgdisk -d 3 $1
         sgdisk -d 2 $1
         sgdisk -n 0::+350G -t 0:8300 -c 0:"Linux filesystem" ${1}
@@ -175,10 +175,10 @@ partitioning() {
         # format
         mkfs.ext4 ${1}2
         mkfs.ext4 ${1}3
-    elif [ ${9} = "no-root-only"]; then
+    elif [ ${9} = "no-root-only" ]; then
         # format
         mkfs.ext4 ${1}2
-    elif [ ${9} = "skip"]; then
+    elif [ ${9} = "skip" ]; then
         echo "Skip partitioning"
 
         # format
@@ -296,17 +296,17 @@ replacement() {
 
 
 boot_loader() {
-    if [ ${10} = "grub"]; then
+    if [ ${10} = "grub" ]; then
         # grub
         arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub --recheck
         arch-chroot /mnt mkdir /boot/EFI/boot
         arch-chroot /mnt cp /boot/EFI/grub/grubx64.efi /boot/EFI/Boot/bootx64.efi
         arch-chroot /mnt sed -i '/^GRUB_TIMEOUT=/c\GRUB_TIMEOUT=10' /etc/default/grub
-        if [ ${4} = "nvidia"]; then
+        if [ ${4} = "nvidia" ]; then
         arch-chroot /mnt sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 panic=180 nomodeset i915.modeset=0 nouveau.modeset=0 nvidia-drm.modeset=1"' /etc/default/grub
-        elif [ ${4} = "amd"]; then
+        elif [ ${4} = "amd" ]; then
             arch-chroot /mnt sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 panic=180"' /etc/default/grub
-        elif [ ${4} = "intel"]; then
+        elif [ ${4} = "intel" ]; then
             arch-chroot /mnt sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 panic=180"' /etc/default/grub
         else
             echo "Missing argument or misspelled..."
@@ -315,7 +315,7 @@ boot_loader() {
         arch-chroot /mnt sed -i '/^GRUB_GFXMODE=/c\GRUB_GFXMODE=1920x1080-24' /etc/default/grub
         arch-chroot /mnt sed -i '/^GRUB_DISABLE_OS_PROBER=/c\GRUB_DISABLE_OS_PROBER=false' /etc/default/grub
         arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-    elif [ ${10} = "systemd-boot"]; then
+    elif [ ${10} = "systemd-boot" ]; then
         # systemd-boot
         arch-chroot /mnt bootctl --path=/boot install
         # echo -e "default    arch\n\
@@ -336,7 +336,7 @@ EOF
         # initrd   /intel-ucode.img\n\
         # initrd   /initramfs-linux-zen.img\n\
         # options  root=PARTUUID=${root_partuuid} rw loglevel=3 panic=180 nomodeset i915.modeset=0 nouveau.modeset=0 nvidia-drm.modeset=1" >> /mnt/boot/loader/entries/arch.conf
-        if [ ${4} = "nvidia"]; then
+        if [ ${4} = "nvidia" ]; then
             cat << EOF >> /mnt/boot/loader/entries/arch.conf
 title    Arch Linux
 linux    /vmlinuz-linux-zen
@@ -344,7 +344,7 @@ initrd   /intel-ucode.img
 initrd   /initramfs-linux-zen.img
 options  root=PARTUUID=${root_partuuid} rw loglevel=3 panic=180 nomodeset i915.modeset=0 nouveau.modeset=0 nvidia-drm.modeset=1
 EOF
-        elif [ ${4} = "amd"]; then
+        elif [ ${4} = "amd" ]; then
             cat << EOF >> /mnt/boot/loader/entries/arch.conf
 title    Arch Linux
 linux    /vmlinuz-linux-zen
@@ -352,7 +352,7 @@ initrd   /intel-ucode.img
 initrd   /initramfs-linux-zen.img
 options  root=PARTUUID=${root_partuuid} rw loglevel=3 panic=180
 EOF
-        elif [ ${4} = "intel"]; then
+        elif [ ${4} = "intel" ]; then
             cat << EOF >> /mnt/boot/loader/entries/arch.conf
 title    Arch Linux
 linux    /vmlinuz-linux-zen
@@ -376,11 +376,11 @@ enable_services() {
     arch-chroot /mnt systemctl enable fstrim.timer
     arch-chroot /mnt systemctl enable ufw.service
 
-    if [ ${3} = "xfce" ] ; then
+    if [ ${3} = "xfce" ]; then
         arch-chroot /mnt systemctl enable lightdm
-    elif [ ${3} = "gnome" ] ; then
+    elif [ ${3} = "gnome" ]; then
         arch-chroot /mnt systemctl enable gdm
-    elif [ ${3} = "kde" ] ; then
+    elif [ ${3} = "kde" ]; then
         arch-chroot /mnt systemctl enable lightdm
     fi
 }
