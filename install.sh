94024e0768ece17e5c769096ff2947a7528a6aeb
reimond2207@gmail.com <remon2207>
2023-01-17 08:43:50 +0900

    update: 2023/01/17 08:42:21

--- install.sh
+++ install.sh
@@ -336,6 +336,7 @@ replacement() {
   arch-chroot /mnt sed -i 's/^--latest 5/# --latest 5/' /etc/xdg/reflector/reflector.conf
   arch-chroot /mnt sed -i 's/^--sort age/--sort rate/' /etc/xdg/reflector/reflector.conf
   arch-chroot /mnt sed -i 's/^#HandlePowerKey=poweroff/HandlePowerKey=ignore/' /etc/systemd/logind.conf
+  arch-chroot /mnt sed -i 's/en_US.UTF-8/ja_JP.UTF-8/' /etc/locale.conf
   arch-chroot /mnt pacman -Syy
 }
 
