27d5017246518a10c899424d5cdc9e0049b0b468
remon2207 <remon2207.develop@gmail.com>
2023-11-05 18:38:55 +0900

    update 2023/11/05 17:45:01

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [[ $# -eq 0 ]]; then
   cat << EOF
 Usage:
 
-# ./install.sh <disk>
+$0 <disk>
   <microcode:intel | amd>
   <DE:i3 | xfce | gnome | kde>
   <GPU:nvidia | amd | intel>
@@ -14,7 +14,7 @@ Usage:
   <UserName>
   <userPasword>
   <rootPassword>
-  <partition-table-destroy:yes | no-exclude-efi | no-root-only | skip>
+  <partition-table-destroy:yes | exclude-efi | root-only | skip>
   <root_partition_size:Numbers only (GiB)>
 EOF
   exit 1
@@ -86,30 +86,104 @@ packagelist="base \
   stylua \
   nfs-utils"
 
-net_interface=$(ip -br link show | grep ' UP ' | awk '{print $1}')
+NET_INTERFACE=$(ip -br link show | grep ' UP ' | awk '{print $1}')
+readonly NET_INTERFACE
 
-disk="${1}"
-microcode="${2}"
-de="${3}"
-gpu="${4}"
-hostname="${5}"
-username="${6}"
-user_password="${7}"
-root_password="${8}"
-partition_table="${9}"
-root_size="${10}"
+IP_ADDRESS=$(ip -o -4 a show "${NET_INTERFACE}" | awk -F '[ /]' '{print $7}')
+readonly IP_ADDRESS
+
+DISK="${1}"
+readonly DISK
+
+MICROCODE="${2}"
+readonly MICROCODE
+
+DE="${3}"
+readonly DE
+
+GPU="${4}"
+readonly GPU
+
+HOSTNAME="${5}"
+readonly HOSTNAME
+
+USERNAME="${6}"
+readonly USERNAME
+
+USER_PASSWORD="${7}"
+readonly USER_PASSWORD
+
+ROOT_PASSWORD="${8}"
+readonly ROOT_PASSWORD
+
+PARTITION_TABLE="${9}"
+readonly PARTITION_TABLE
+
+ROOT_SIZE="${10}"
+readonly ROOT_SIZE
+
+VCONSOLE=$(
+  cat << EOF
+KEYMAP=us
+FONT=drdos8x16
+EOF
+)
+readonly VCONSOLE
+
+ENVIRONMENT=$(
+  cat << EOF
+GTK_IM_MODULE='fcitx5'
+QT_IM_MODULE='fcitx5'
+XMODIFIERS='@im=fcitx5'
+
+LIBVA_DRIVER_NAME='nvidia'
+VDPAU_DRIVER='nvidia'
+EOF
+)
+readonly ENVIRONMENT
+
+LOADER_CONF=$(
+  cat << EOF
+default      arch.conf
+timeout      10
+console-mode max
+editor       no
+EOF
+)
+readonly LOADER_CONF
+
+HOSTS=$(
+  cat << EOF
+127.0.0.1       localhost
+::1             localhost
+${IP_ADDRESS}   ${HOSTNAME}.home    ${HOSTNAME}
+EOF
+)
+readonly HOSTS
+
+WIRED=$(
+  cat << EOF
+[Match]
+Name=${NET_INTERFACE}
+
+[Network]
+DHCP=yes
+DNS=192.168.1.202
+EOF
+)
+readonly WIRED
 
 check_variables() {
-  if [[ "${microcode}" != 'intel' ]] && [[ "${microcode}" != 'amd' ]]; then
+  if [[ "${MICROCODE}" != 'intel' ]] && [[ "${MICROCODE}" != 'amd' ]]; then
     echo 'microcode error'
     exit 1
-  elif [[ "${de}" != 'i3' ]] && [[ "${de}" != 'xfce' ]] && [[ "${de}" != 'gnome' ]] && [[ "${de}" != 'kde' ]]; then
+  elif [[ "${DE}" != 'i3' ]] && [[ "${DE}" != 'xfce' ]] && [[ "${DE}" != 'gnome' ]] && [[ "${DE}" != 'kde' ]]; then
     echo 'de error'
     exit 1
-  elif [[ "${gpu}" != 'nvidia' ]] && [[ "${gpu}" != 'amd' ]] && [[ "${gpu}" != 'intel' ]]; then
+  elif [[ "${GPU}" != 'nvidia' ]] && [[ "${GPU}" != 'amd' ]] && [[ "${GPU}" != 'intel' ]]; then
     echo 'gpu error'
     exit 1
-  elif [[ "${partition_table}" != 'yes' ]] && [[ "${partition_table}" != 'no-exclude-efi' ]] && [[ "${partition_table}" != 'no-root-only' ]] && [[ "${partition_table}" != 'skip' ]]; then
+  elif [[ "${PARTITION_TABLE}" != 'yes' ]] && [[ "${PARTITION_TABLE}" != 'exclude-efi' ]] && [[ "${PARTITION_TABLE}" != 'root-only' ]] && [[ "${PARTITION_TABLE}" != 'skip' ]]; then
     echo 'partition table error'
     exit 1
   fi
@@ -117,14 +191,14 @@ check_variables() {
 
 selection_arguments() {
   # intel-ucode or amd-ucode
-  if [[ "${microcode}" == 'intel' ]]; then
+  if [[ "${MICROCODE}" == 'intel' ]]; then
     packagelist="${packagelist} intel-ucode"
-  elif [[ "${microcode}" == 'amd' ]]; then
+  elif [[ "${MICROCODE}" == 'amd' ]]; then
     packagelist="${packagelist} amd-ucode"
   fi
 
   # DE
-  if [[ "${de}" == 'i3' ]]; then
+  if [[ "${DE}" == 'i3' ]]; then
     packagelist="${packagelist} \
       i3-wm \
       i3lock \
@@ -149,7 +223,7 @@ selection_arguments() {
       tmux \
       ttf-font-awesome \
       ranger"
-  elif [[ "${de}" == 'xfce' ]]; then
+  elif [[ "${DE}" == 'xfce' ]]; then
     packagelist="${packagelist} \
       xfce4 \
       xfce4-goodies \
@@ -163,7 +237,7 @@ selection_arguments() {
       lightdm \
       lightdm-gtk-greeter \
       lightdm-gtk-greeter-settings"
-  elif [[ "${de}" == 'gnome' ]]; then
+  elif [[ "${DE}" == 'gnome' ]]; then
     packagelist="${packagelist} \
       gnome-control-center \
       gnome-shell \
@@ -183,7 +257,7 @@ selection_arguments() {
       eog \
       networkmanager \
       gnome-shell-extension-appindicator"
-  elif [[ "${de}" == 'kde' ]]; then
+  elif [[ "${DE}" == 'kde' ]]; then
     packagelist="${packagelist} \
       plasma-meta \
       packagekit-qt5 \
@@ -194,9 +268,9 @@ selection_arguments() {
       kate"
   fi
 
-  if [[ "${gpu}" == 'nvidia' ]]; then
+  if [[ "${GPU}" == 'nvidia' ]]; then
     packagelist="${packagelist} nvidia-dkms nvidia-settings"
-  elif [[ "${gpu}" == 'amd' ]]; then
+  elif [[ "${GPU}" == 'amd' ]]; then
     packagelist="${packagelist} xf86-video-amdgpu libva-mesa-driver mesa-vdpau"
   fi
 }
@@ -206,40 +280,40 @@ time_setting() {
 }
 
 partitioning() {
-  if [[ "${partition_table}" == 'yes' ]]; then
-    sgdisk -Z "${disk}"
-    sgdisk -n 0::+512M -t 0:ef00 -c '0:EFI system partition' "${disk}"
-    sgdisk -n "0::+${root_size}G" -t 0:8300 -c '0:Linux filesystem' "${disk}"
-    sgdisk -n 0:: -t 0:8300 -c '0:Linux filesystem' "${disk}"
+  if [[ "${PARTITION_TABLE}" == 'yes' ]]; then
+    sgdisk -Z "${DISK}"
+    sgdisk -n 0::+512M -t 0:ef00 -c '0:EFI system partition' "${DISK}"
+    sgdisk -n "0::+${ROOT_SIZE}G" -t 0:8300 -c '0:Linux filesystem' "${DISK}"
+    sgdisk -n 0:: -t 0:8300 -c '0:Linux filesystem' "${DISK}"
 
     # format
-    mkfs.fat -F 32 "${disk}1"
-    mkfs.ext4 "${disk}2"
-    mkfs.ext4 "${disk}3"
-  elif [[ "${partition_table}" == 'no-exclude-efi' ]]; then
-    sgdisk -d 3 "${disk}"
-    sgdisk -d 2 "${disk}"
-    sgdisk -n "0::+${root_size}G" -t 0:8300 -c '0:EFI system partition' "${disk}"
-    sgdisk -n 0:: -t 0:8300 -c '0:Linux filesystem' "${disk}"
+    mkfs.fat -F 32 "${DISK}1"
+    mkfs.ext4 "${DISK}2"
+    mkfs.ext4 "${DISK}3"
+  elif [[ "${PARTITION_TABLE}" == 'exclude-efi' ]]; then
+    sgdisk -d 3 "${DISK}"
+    sgdisk -d 2 "${DISK}"
+    sgdisk -n "0::+${ROOT_SIZE}G" -t 0:8300 -c '0:EFI system partition' "${DISK}"
+    sgdisk -n 0:: -t 0:8300 -c '0:Linux filesystem' "${DISK}"
 
     # format
-    mkfs.ext4 "${disk}2"
-    mkfs.ext4 "${disk}3"
-  elif [[ "${partition_table}" == 'no-root-only' ]]; then
+    mkfs.ext4 "${DISK}2"
+    mkfs.ext4 "${DISK}3"
+  elif [[ "${PARTITION_TABLE}" == 'root-only' ]]; then
     # format
-    mkfs.ext4 "${disk}2"
-  elif [[ "${partition_table}" == 'skip' ]]; then
+    mkfs.ext4 "${DISK}2"
+  elif [[ "${PARTITION_TABLE}" == 'skip' ]]; then
     echo 'Skip partitioning'
 
     # format
-    mkfs.ext4 "${disk}2"
-    mkfs.ext4 "${disk}3"
+    mkfs.ext4 "${DISK}2"
+    mkfs.ext4 "${DISK}3"
   fi
 
   # mount
-  mount "${disk}2" /mnt
-  mount -m -o fmask=0077,dmask=0077 "${disk}1" /mnt/boot
-  mount -m "${disk}3" /mnt/home
+  mount "${DISK}2" /mnt
+  mount -m -o fmask=0077,dmask=0077 "${DISK}1" /mnt/boot
+  mount -m "${DISK}3" /mnt/home
 }
 
 installation() {
@@ -257,51 +331,38 @@ configuration() {
   arch-chroot /mnt sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
   arch-chroot /mnt locale-gen
   echo 'LANG=en_US.UTF-8' > /mnt/etc/locale.conf
-  echo 'KEYMAP=us' > /mnt/etc/vconsole.conf
-  echo "${hostname}" > /mnt/etc/hostname
+  echo "${VCONSOLE}" >> /mnt/etc/vconsole.conf
+  echo "${HOSTNAME}" > /mnt/etc/hostname
   arch-chroot /mnt sh -c "echo '%wheel ALL=(ALL:ALL) ALL' | EDITOR='tee -a' visudo"
 }
 
 networking() {
-  ip_address=$(ip -4 a show "${net_interface}" | grep '192.168' | awk '{print $2}' | cut -d '/' -f 1)
-  cat << EOF >> /mnt/etc/hosts
-127.0.0.1       localhost
-::1             localhost
-${ip_address}    ${hostname}.home    ${hostname}
-EOF
+  echo "${HOSTS}" >> /mnt/etc/hosts
 
-  if [[ "${de}" != 'gnome' ]] && [[ "${de}" != 'kde' ]]; then
-    arch-chroot /mnt systemctl enable systemd-{networkd,resolved}.service
-    cat << EOF > /mnt/etc/systemd/network/20-wired.network
-[Match]
-Name=${net_interface}
-
-[Network]
-DHCP=yes
-DNS=192.168.1.202
-EOF
-  elif [[ "${de}" != 'i3' ]]; then
-    arch-chroot /mnt systemctl enable systemd-resolved.service
-    ln -sf ../run/NetworkManager/no-stub-resolv.conf /mnt/etc/resolv.conf
+  if [[ "${DE}" == 'i3' ]] || [[ "${DE}" == 'xfce' ]]; then
+    ln -sf /run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
+    echo "${WIRED}" > /mnt/etc/systemd/network/20-wired.network
   else
-    ln -sf ../run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
+    ln -sf /run/NetworkManager/no-stub-resolv.conf /mnt/etc/resolv.conf
   fi
 }
 
 create_user() {
-  echo "root:${root_password}" | arch-chroot /mnt chpasswd
-  arch-chroot /mnt useradd -m -G wheel -s /bin/bash "${username}"
-  echo "${username}:${user_password}" | arch-chroot /mnt chpasswd
+  echo "root:${ROOT_PASSWORD}" | arch-chroot /mnt chpasswd
+  arch-chroot /mnt useradd -m -G wheel -s /bin/bash "${USERNAME}"
+  echo "${USERNAME}:${USER_PASSWORD}" | arch-chroot /mnt chpasswd
 }
 
 add_to_group() {
-  arch-chroot /mnt gpasswd -a "${username}" docker
-  arch-chroot /mnt gpasswd -a "${username}" vboxusers
+  arch-chroot /mnt gpasswd -a "${USERNAME}" docker
+  arch-chroot /mnt gpasswd -a "${USERNAME}" vboxusers
 }
 
 replacement() {
   arch-chroot /mnt sed -i 's/^#NTP=/NTP=ntp.nict.jp/' /etc/systemd/timesyncd.conf
   arch-chroot /mnt sed -i 's/^#FallbackNTP=/FallbackNTP=ntp1.jst.mfeed.ad.jp ntp2.jst.mfeed.ad.jp ntp3.jst.mfeed.ad.jp/' /etc/systemd/timesyncd.conf
+  arch-chroot /mnt sed -i 's/^#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=10s/' /etc/systemd/system.conf
+  arch-chroot /mnt sed -i 's/^#HandlePowerKey=poweroff/HandlePowerKey=ignore/' /etc/systemd/logind.conf
   arch-chroot /mnt sed -i 's/-march=x86-64 -mtune=generic/-march=native/' /etc/makepkg.conf
   arch-chroot /mnt sed -i 's/^#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(($(nproc)+1))"/' /etc/makepkg.conf
   arch-chroot /mnt sed -i 's/^#BUILDDIR/BUILDDIR/' /etc/makepkg.conf
@@ -309,86 +370,110 @@ replacement() {
   arch-chroot /mnt sed -i 's/^COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q --threads=0 -)/' /etc/makepkg.conf
   arch-chroot /mnt sed -i 's/^COMPRESSGZ=(gzip -c -f -n)/COMPRESSGZ=(pigz -c -f -n)/' /etc/makepkg.conf
   arch-chroot /mnt sed -i 's/^COMPRESSBZ2=(bzip2 -c -f)/COMPRESSBZ2=(lbzip2 -c -f)/' /etc/makepkg.conf
-  arch-chroot /mnt sed -i 's/^#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=10s/' /etc/systemd/system.conf
   arch-chroot /mnt sed -i 's/^#Color/Color/' /etc/pacman.conf
   arch-chroot /mnt sed -i 's/^# --country France,Germany/--country Japan/' /etc/xdg/reflector/reflector.conf
   arch-chroot /mnt sed -i 's/^--latest 5/# --latest 5/' /etc/xdg/reflector/reflector.conf
   arch-chroot /mnt sed -i 's/^--sort age/--sort rate/' /etc/xdg/reflector/reflector.conf
-  cat << EOF >> /mnt/etc/xdg/reflector/reflector.conf
-
---age 24
-EOF
-  arch-chroot /mnt sed -i 's/^#HandlePowerKey=poweroff/HandlePowerKey=ignore/' /etc/systemd/logind.conf
-  cat << EOF >> /mnt/etc/environment
-GTK_IM_MODULE='fcitx5'
-QT_IM_MODULE='fcitx5'
-XMODIFIERS='@im=fcitx5'
-
-LIBVA_DRIVER_NAME='nvidia'
-VDPAU_DRIVER='nvidia'
-EOF
-
+  echo -e '\n--age 24' /mnt/etc/xdg/reflector/reflector.conf
+  echo "${ENVIRONMENT}" >> /mnt/etc/environment
   arch-chroot /mnt pacman -Syy
 }
 
 boot_loader() {
   arch-chroot /mnt bootctl install
-  cat << EOF > /mnt/boot/loader/loader.conf
-default      arch.conf
-timeout      10
-console-mode max
-editor       no
-EOF
 
-  root_partuuid=$(blkid -s PARTUUID -o value "${disk}2")
+  root_partuuid=$(blkid -s PARTUUID -o value "${DISK}2")
+  vmlinuz=$(find /boot/*vmlinuz* | awk -F '/' '{print $3}')
+  ucode=$(find /boot/*ucode* | awk -F '/' '{print $3}')
+  initramfs=$(find /boot/*initramfs* | tail -n 1 | awk -F '/' '{print $3}')
+  initramfs_fallback=$(find /boot/*initramfs* | head -n 1)
 
-  if [[ "${gpu}" == 'nvidia' ]]; then
-    cat << EOF > /mnt/boot/loader/entries/arch.conf
+  nvidia_conf=$(
+    cat << EOF
 title    Arch Linux
-linux    /vmlinuz-linux-zen
-initrd   /intel-ucode.img
-initrd   /initramfs-linux-zen.img
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs}
 options  root=PARTUUID=${root_partuuid} rw loglevel=3 panic=180 i915.modeset=0 nouveau.modeset=0 nvidia_drm.modeset=1
 EOF
-  elif [[ "${gpu}" == 'amd' ]]; then
-    cat << EOF > /mnt/boot/loader/entries/arch.conf
-title    Arch Linux
-linux    /vmlinuz-linux-zen
-initrd   /amd-ucode.img
-initrd   /initramfs-linux-zen.img
-options  root=PARTUUID=${root_partuuid} rw loglevel=3 panic=180
-EOF
-  elif [[ "${gpu}" == 'intel' ]]; then
-    cat << EOF > /mnt/boot/loader/entries/arch.conf
-title    Arch Linux
-linux    /vmlinuz-linux-zen
-initrd   /intel-ucode.img
-initrd   /initramfs-linux-zen.img
-options  root=PARTUUID=${root_partuuid} rw loglevel=3 panic=180
-EOF
-  fi
-
-  cat << EOF > /mnt/boot/loader/entries/arch_fallback.conf
+  )
+  nvidia_fallback_conf=$(
+    cat << EOF
 title    Arch Linux (Fallback)
-linux    /vmlinuz-linux-zen
-initrd   /intel-ucode.img
-initrd   /initramfs-linux-zen-fallback.img
-options  root=PARTUUID=${root_partuuid} rw panic=180 debug i915.modeset=0 nouveau.modeset=0 nvidia_drm.modeset=1
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs_fallback}
+options  root=PARTUUID=${root_partuuid} rw debug panic=180 i915.modeset=0 nouveau.modeset=0 nvidia_drm.modeset=1
 EOF
+  )
+  amd_conf=$(
+    cat << EOF
+title    Arch Linux
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs}
+options  root=PARTUUID=${root_partuuid} rw loglevel=3 panic=180
+EOF
+  )
+  amd_fallback_conf=$(
+    cat << EOF
+title    Arch Linux (Fallback)
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs_fallback}
+options  root=PARTUUID=${root_partuuid} rw debug panic=180 i915.modeset=0
+EOF
+  )
+  INTEL_CONF=$(
+    cat << EOF
+title    Arch Linux
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs}
+options  root=PARTUUID=${root_partuuid} rw loglevel=3 panic=180
+EOF
+  )
+  local -r INTEL_CONF
+  INTEL_FALLBACK_CONF=$(
+    cat << EOF
+title    Arch Linux (Fallback)
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs_fallback}
+options  root=PARTUUID=${root_partuuid} rw debug panic=180
+EOF
+  )
+  local -r INTEL_FALLBACK_CONF
+
+  echo "${LOADER_CONF}" > /mnt/boot/loader/loader.conf
+  if [[ "${GPU}" == 'nvidia' ]]; then
+    echo "${nvidia_conf}" > /mnt/boot/loader/entries/arch.conf
+    echo "${nvidia_fallback_conf}" > /mnt/boot/loader/entries/arch_fallback.conf
+  elif [[ "${GPU}" == 'amd' ]]; then
+    echo "${amd_conf}" > /mnt/boot/loader/entries/arch.conf
+    echo "${amd_fallback_conf}" > /mnt/boot/loader/entries/arch_fallback.conf
+  elif [[ "${GPU}" == 'intel' ]]; then
+    echo "${INTEL_CONF}" > /mnt/boot/loader/entries/arch.conf
+    echo "${INTEL_FALLBACK_CONF}" > /mnt/boot/loader/entries/arch_fallback.conf
+  fi
 }
 
 enable_services() {
+  arch-chroot /mnt systemctl enable iptables.service
   arch-chroot /mnt systemctl enable docker.service
   arch-chroot /mnt systemctl enable fstrim.timer
   arch-chroot /mnt systemctl enable reflector.timer
   arch-chroot /mnt systemctl enable systemd-boot-update.service
-
-  if [[ "${de}" == 'xfce' ]]; then
+  if [[ "${DE}" == 'i3' ]] || [[ "${DE}" == 'xfce' ]]; then
+    arch-chroot /mnt systemctl enable systemd-networkd.service
+    arch-chroot /mnt systemctl enable systemd-resolved.service
+  fi
+  if [[ "${DE}" == 'xfce' ]]; then
     arch-chroot /mnt systemctl enable lightdm.service
-  elif [[ "${de}" == 'gnome' ]]; then
+  elif [[ "${DE}" == 'gnome' ]]; then
     arch-chroot /mnt systemctl enable gdm.service
     arch-chroot /mnt systemctl enable NetworkManager.service
-  elif [[ "${de}" == 'kde' ]]; then
+  elif [[ "${DE}" == 'kde' ]]; then
     arch-chroot /mnt systemctl enable sddm.service
     arch-chroot /mnt systemctl enable NetworkManager.service
   fi
