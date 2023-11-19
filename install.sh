3a2da4f470cefde93ce985eba33e4801e99dd556
remon2207 <remon2207.develop@gmail.com>
2023-11-19 15:06:19 +0900

    update 2023/11/19 14:52:03

--- install.sh
+++ install.sh
@@ -369,7 +369,8 @@ configuration() {
   echo 'LANG=en_US.UTF-8' > /mnt/etc/locale.conf
   echo 'KEYMAP=us' >> /mnt/etc/vconsole.conf
   echo "${HOST_NAME}" > /mnt/etc/hostname
-  arch-chroot /mnt bash -c "echo '%wheel ALL=(ALL:ALL) ALL' | EDITOR='tee -a' visudo"
+  # arch-chroot /mnt bash -c "echo '%wheel ALL=(ALL:ALL) ALL' | EDITOR='tee -a' visudo"
+  arch-chroot /mnt sed -i -e 's/^# \(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers | EDITOR='tee' arch-chroot /mnt visudo
 }
 
 networking() {
