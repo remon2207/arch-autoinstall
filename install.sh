58f1bff77069f2fe9581e3524b2a7ce9e0e3fca4
remon2207 <remon2207.develop@gmail.com>
2024-01-12 02:02:53 +0900

    2024/01/12 02:02:53

--- install.sh
+++ install.sh
@@ -26,11 +26,11 @@ EOF
 
 unalias -a
 
-to-arch() { arch-chroot /mnt "${@}"; }
+to_arch() { arch-chroot /mnt "${@}"; }
 
 readonly KERNEL='linux-zen'
 readonly USER_NAME='remon'
-CPU_INFO="$(grep 'model name' /proc/cpuinfo | awk --field-separator='[ (]' 'NR==1 {print $3}')" && readonly CPU_INFO
+readonly CPU_INFO && CPU_INFO="$(grep 'model name' /proc/cpuinfo | awk --field-separator='[ (]' 'NR==1 {print $3}')"
 
 packagelist="base \
   base-devel \
@@ -73,6 +73,7 @@ packagelist="base \
   ttf-hack \
   fcitx5-im \
   fcitx5-mozc \
+  up \
   docker \
   docker-compose \
   github-cli \
@@ -257,22 +258,21 @@ time_setting() {
 }
 
 partitioning() {
-  local -r NORMAL_PART_TYPE="$(sgdisk --list-types \
+  local -r normal_part_type="$(sgdisk --list-types \
     | grep '8300' \
     | awk '{print $2,$3}')"
 
   case "${PARTITION_DESTROY}" in
     'yes')
-      local -r EFI_PART_TYPE="$(sgdisk --list-types \
+      local -r efi_part_type="$(sgdisk --list-types \
         | grep 'ef00' \
         | awk '{print $6,$7,$8}')"
 
       sgdisk --zap-all "${DISK}"
-      sgdisk --new='0::+512M' --typecode='0:ef00' --change-name="0:${EFI_PART_TYPE}" "${DISK}"
-      sgdisk --new="0::+${ROOT_SIZE}G" --typecode='0:8300' --change-name="0:${NORMAL_PART_TYPE}" "${DISK}"
-      sgdisk --new='0::' --typecode='0:8300' --change-name="0:${NORMAL_PART_TYPE}" "${DISK}"
+      sgdisk --new='0::+512M' --typecode='0:ef00' --change-name="0:${efi_part_type}" "${DISK}"
+      sgdisk --new="0::+${ROOT_SIZE}G" --typecode='0:8300' --change-name="0:${normal_part_type}" "${DISK}"
+      sgdisk --new='0::' --typecode='0:8300' --change-name="0:${normal_part_type}" "${DISK}"
 
-      # format
       mkfs.fat -F 32 "${DISK}1"
       mkfs.ext4 "${DISK}2"
       mkfs.ext4 "${DISK}3"
@@ -280,35 +280,31 @@ partitioning() {
     'exclude-efi')
       sgdisk --delete=3 "${DISK}"
       sgdisk --delete=2 "${DISK}"
-      sgdisk --new="0::+${ROOT_SIZE}G" --typecode='0:8300' --change-name="0:${NORMAL_PART_TYPE}" "${DISK}"
-      sgdisk --new='0::' --typecode='0:8300' --change-name="0:${NORMAL_PART_TYPE}" "${DISK}"
+      sgdisk --new="0::+${ROOT_SIZE}G" --typecode='0:8300' --change-name="0:${normal_part_type}" "${DISK}"
+      sgdisk --new='0::' --typecode='0:8300' --change-name="0:${normal_part_type}" "${DISK}"
 
-      # format
       mkfs.ext4 "${DISK}2"
       mkfs.ext4 "${DISK}3"
       ;;
     'root-only')
-      # format
       mkfs.ext4 "${DISK}2"
       ;;
     'skip')
-      # format
       mkfs.ext4 "${DISK}2"
       mkfs.ext4 "${DISK}3"
       ;;
   esac
 
-  # mount
   mount "${DISK}2" /mnt
   mount --mkdir "${DISK}1" /mnt/boot
   mount --mkdir "${DISK}3" /mnt/home
 }
 
 installation() {
-  local -r NUMBER_HOOKS="$(grep --line-number '^HOOKS' /etc/mkinitcpio.conf)"
-  local -r NUMBER="$(echo "${NUMBER_HOOKS}" | awk --field-separator=':' '{print $1}')"
-  local -r HOOKS_ORG="$(echo "${NUMBER_HOOKS}" | awk --field-separator=':' '{print $2}')"
-  local -r NEW_NUMBER="$(("${NUMBER}" + 1))"
+  local -r number_hooks="$(grep --line-number '^HOOKS' /etc/mkinitcpio.conf)"
+  local -r number="$(echo "${number_hooks}" | awk --field-separator=':' '{print $1}')"
+  local -r hooks_org="$(echo "${number_hooks}" | awk --field-separator=':' '{print $2}')"
+  local -r new_number="$(("${number}" + 1))"
 
   reflector --country='Japan' --age=24 --protocol='https' --sort='rate' --save='/etc/pacman.d/mirrorlist'
   sed --in-place --expression='s/^#\(ParallelDownloads\)/\1/' /etc/pacman.conf
@@ -317,77 +313,75 @@ installation() {
 
   case "${GPU}" in
     'nvidia')
-      # shellcheck disable=SC2001
-      local -r NVIDIA_HOOKS="$(echo "${HOOKS_ORG}" | sed --expression='s/\(.*\)kms \(.*\)consolefont \(.*\)/\1\2\3/')"
+      local -r nvidia_hooks="$(echo "${hooks_org}" | sed --expression='s/\(.*\)kms \(.*\)consolefont \(.*\)/\1\2\3/')"
 
-      to-arch sed --in-place \
+      to_arch sed --in-place \
         --expression='s/^MODULES=(/&nvidia nvidia_modeset nvidia_uvm nvidia_drm/' \
-        --expression="${NUMBER}s/^/#/" \
-        --expression="${NEW_NUMBER}i ${NVIDIA_HOOKS}" /etc/mkinitcpio.conf
+        --expression="${number}s/^/#/" \
+        --expression="${new_number}i ${nvidia_hooks}" /etc/mkinitcpio.conf
       ;;
     'amd')
-      # shellcheck disable=SC2001
-      local -r AMD_HOOKS="$(echo "${HOOKS_ORG}" | sed --expression='s/\(.*\)consolefont \(.*\)/\1\2/')"
+      local -r amd_hooks="$(echo "${hooks_org}" | sed --expression='s/\(.*\)consolefont \(.*\)/\1\2/')"
 
-      to-arch sed --in-place \
-        --expression="${NUMBER}s/^/#/" \
-        --expression="${NEW_NUMBER}i ${AMD_HOOKS}" /etc/mkinitcpio.conf
+      to_arch sed --in-place \
+        --expression="${number}s/^/#/" \
+        --expression="${new_number}i ${amd_hooks}" /etc/mkinitcpio.conf
       ;;
   esac
 
-  to-arch mkinitcpio -p "${KERNEL}"
+  to_arch mkinitcpio -p "${KERNEL}"
   genfstab -t 'PARTUUID' /mnt >> /mnt/etc/fstab
 }
 
 configuration() {
-  to-arch reflector --country='Japan' --age=24 --protocol='https' --sort='rate' --save='/etc/pacman.d/mirrorlist'
-  to-arch ln --symbolic --force /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
-  to-arch hwclock --systohc --utc
-  to-arch sed --in-place \
+  to_arch reflector --country='Japan' --age=24 --protocol='https' --sort='rate' --save='/etc/pacman.d/mirrorlist'
+  to_arch ln --symbolic --force /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+  to_arch hwclock --systohc --utc
+  to_arch sed --in-place \
     --expression='s/^#\(en_US.UTF-8 UTF-8\)/\1/' \
     --expression='s/^#\(ja_JP.UTF-8 UTF-8\)/\1/' /etc/locale.gen
-  to-arch sed --in-place --expression='s/^#\(ParallelDownloads\)/\1/' /etc/pacman.conf
-  to-arch locale-gen
+  to_arch sed --in-place --expression='s/^#\(ParallelDownloads\)/\1/' /etc/pacman.conf
+  to_arch locale-gen
   echo 'LANG=en_US.UTF-8' > /mnt/etc/locale.conf
   echo 'KEYMAP=us' >> /mnt/etc/vconsole.conf
   echo 'archlinux' > /mnt/etc/hostname
-  to-arch sed --expression='s/^# \(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers | EDITOR='/usr/bin/tee' to-arch visudo &> /dev/null
+  to_arch sed --expression='s/^# \(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers | EDITOR='/usr/bin/tee' to_arch visudo &> /dev/null
 }
 
 networking() {
-  local -r NET_INTERFACE="$(ip -br link show \
+  local -r net_interface="$(ip -br link show \
     | grep ' UP ' \
     | awk '{print $1}')"
 
-  local -r HOSTS="$(
+  local -r hosts="$(
     cat << EOF
 127.0.0.1       localhost
 ::1             localhost
 EOF
   )"
 
-  local -r WIRED="[Match]
-Name=${NET_INTERFACE}
+  local -r wired="[Match]
+Name=${net_interface}
 
 [Network]
 DHCP=yes"
 
-  local -r RESOLVED="[Resolve]
+  local -r resolved="[Resolve]
 DNS=192.168.1.202"
 
-  local -r RESOLVED_FALLBACK="[Resolve]
-FallbackDNS=2001:4860:4860::8888 2001:4860:4860::8844 8.8.8.8 8.8.4.4"
+  local -r resolved_fallback="[Resolve]
+FallbackDNS=2400:4053:2b41:2a00:280:87ff:fef7:86fa 192.168.1.1"
 
-  echo "${HOSTS}" >> /mnt/etc/hosts
+  echo "${hosts}" >> /mnt/etc/hosts
 
   case "${DE}" in
     'i3' | 'xfce')
-      to-arch mkdir /etc/systemd/resolved.conf.d
+      to_arch mkdir /etc/systemd/resolved.conf.d
       ln --symbolic --force /run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
 
-      echo "${WIRED}" > /mnt/etc/systemd/network/20-wired.network
-      echo "${RESOLVED}" > /mnt/etc/systemd/resolved.conf.d/dns_servers.conf
-      echo "${RESOLVED_FALLBACK}" > /mnt/etc/systemd/resolved.conf.d/fallback_dns.conf
+      echo "${wired}" > /mnt/etc/systemd/network/20-wired.network
+      echo "${resolved}" > /mnt/etc/systemd/resolved.conf.d/dns_servers.conf
+      echo "${resolved_fallback}" > /mnt/etc/systemd/resolved.conf.d/fallback_dns.conf
       ;;
     *)
       ln --symbolic --force /run/NetworkManager/no-stub-resolv.conf /mnt/etc/resolv.conf
@@ -396,19 +390,20 @@ FallbackDNS=2001:4860:4860::8888 2001:4860:4860::8844 8.8.8.8 8.8.4.4"
 }
 
 create_user() {
-  echo "root:${ROOT_PASSWORD}" | to-arch chpasswd
-  to-arch useradd --create-home --groups='wheel' --shell='/bin/bash' "${USER_NAME}"
-  echo "${USER_NAME}:${USER_PASSWORD}" | to-arch chpasswd
+  echo "root:${ROOT_PASSWORD}" | to_arch chpasswd
+  to_arch useradd --create-home --groups='wheel' --shell='/bin/bash' "${USER_NAME}"
+  echo "${USER_NAME}:${USER_PASSWORD}" | to_arch chpasswd
 }
 
 add_to_group() {
-  for groups in docker vboxusers; do to-arch gpasswd --add "${USER_NAME}" "${groups}"; done
+  local groups
+  for groups in docker vboxusers; do to_arch gpasswd --add "${USER_NAME}" "${groups}"; done
 }
 
 replacement() {
   case "${GPU}" in
     'nvidia')
-      local -r ENVIRONMENT="GTK_IM_MODULE='fcitx5'
+      local -r environment="GTK_IM_MODULE='fcitx5'
 QT_IM_MODULE='fcitx5'
 XMODIFIERS='@im=fcitx5'
 GLFW_IM_MODULE='ibus'
@@ -417,7 +412,8 @@ LIBVA_DRIVER_NAME='vdpau'
 VDPAU_DRIVER='nvidia'"
       ;;
     'amd')
-      local -r ENVIRONMENT="GTK_IM_MODULE='fcitx5'
+      # shellcheck disable=2034
+      local -r environment="GTK_IM_MODULE='fcitx5'
 QT_IM_MODULE='fcitx5'
 XMODIFIERS='@im=fcitx5'
 GLFW_IM_MODULE='ibus'
@@ -427,15 +423,15 @@ VDPAU_DRIVER='radeonsi'"
       ;;
   esac
 
-  to-arch sed --in-place \
+  to_arch sed --in-place \
     --expression='s/^#\(NTP=\)/\1ntp.nict.jp/' \
     --expression='s/^#\(FallbackNTP=\).*/\1ntp1.jst.mfeed.ad.jp ntp2.jst.mfeed.ad.jp ntp3.jst.mfeed.ad.jp/' /etc/systemd/timesyncd.conf
-  to-arch sed --in-place \
+  to_arch sed --in-place \
     --expression='s/^# \(--country\) France,Germany/\1 Japan/' \
     --expression='s/^--latest 5/# &/' \
     --expression='s/^\(--sort\) age/\1 rate/' /etc/xdg/reflector/reflector.conf
   # shellcheck disable=SC2016
-  to-arch sed --in-place \
+  to_arch sed --in-place \
     --expression='s/\(-march=\)x86-64 -mtune=generic/\1skylake/' \
     --expression='s/^#\(MAKEFLAGS=\).*/\1"-j$(($(nproc)+1))"/' \
     --expression='s/^#\(BUILDDIR\)/\1/' \
@@ -443,30 +439,30 @@ VDPAU_DRIVER='radeonsi'"
     --expression='s/^\(COMPRESSZST=\)(zstd -c -z -q -)/\1(zstd -c -z -q --threads=0 -)/' \
     --expression='s/^\(COMPRESSGZ=\)(gzip -c -f -n)/\1(pigz -c -f -n)/' \
     --expression='s/^\(COMPRESSBZ2=\)(bzip2 -c -f)/\1(lbzip2 -c -f)/' /etc/makepkg.conf
-  to-arch sed --in-place --expression='s/^#\(HandlePowerKey=\).*/\1reboot/' /etc/systemd/logind.conf
-  to-arch sed --in-place --expression='s/^#\(DefaultTimeoutStopSec=\).*/\110s/' /etc/systemd/system.conf
-  to-arch sed --in-place --expression='s/^#\(Color\)/\1/' /etc/pacman.conf
+  to_arch sed --in-place --expression='s/^#\(HandlePowerKey=\).*/\1reboot/' /etc/systemd/logind.conf
+  to_arch sed --in-place --expression='s/^#\(DefaultTimeoutStopSec=\).*/\110s/' /etc/systemd/system.conf
+  to_arch sed --in-place --expression='s/^#\(Color\)/\1/' /etc/pacman.conf
   echo -e '\n--age 24' >> /mnt/etc/xdg/reflector/reflector.conf
-  echo "${ENVIRONMENT}" >> /mnt/etc/environment
+  echo "${environment}" >> /mnt/etc/environment
 
-  to-arch pacman --sync --refresh --refresh
+  to_arch pacman --sync --refresh --refresh
 }
 
 boot_loader() {
   find_boot() { find /mnt/boot -type 'f' -name "${1}"; }
 
-  to-arch bootctl install
+  to_arch bootctl install
 
-  local -r ROOT_PARTUUID="$(blkid --match-tag='PARTUUID' --output='value' "${DISK}2")"
-  local -r VMLINUZ="$(find_boot "*vmlinuz*${KERNEL}*" | awk --field-separator='/' '{print $4}')"
-  local -r UCODE="$(find_boot '*ucode*' | awk --field-separator='/' '{print $4}')"
-  local -r INITRAMFS="$(find_boot "*initramfs*${KERNEL}*" | awk --field-separator='/' 'NR==1 {print $4}')"
-  local -r INITRAMFS_FALLBACK="$(find_boot "*initramfs*${KERNEL}*" | awk --field-separator='/' 'END {print $4}')"
-  local -r NVIDIA_PARAMS='rw panic=180 nvidia_drm.modeset=1'
-  local -r AMD_PARAMS='rw panic=180'
-  local -r ENTRIES='/mnt/boot/loader/entries'
+  local -r root_partuuid="$(blkid --match-tag='PARTUUID' --output='value' "${DISK}2")"
+  local -r vmlinuz="$(find_boot "*vmlinuz*${KERNEL}*" | awk --field-separator='/' '{print $4}')"
+  local -r ucode="$(find_boot '*ucode*' | awk --field-separator='/' '{print $4}')"
+  local -r initramfs="$(find_boot "*initramfs*${KERNEL}*" | awk --field-separator='/' 'NR==1 {print $4}')"
+  local -r initramfs_fallback="$(find_boot "*initramfs*${KERNEL}*" | awk --field-separator='/' 'END {print $4}')"
+  local -r nvidia_params='rw panic=180 nvidia_drm.modeset=1'
+  local -r amd_params='rw panic=180'
+  local -r entries='/mnt/boot/loader/entries'
 
-  local -r LOADER_CONF="$(
+  local -r loader_conf="$(
     cat << EOF
 timeout      15
 console-mode max
@@ -474,75 +470,75 @@ editor       no
 EOF
   )"
 
-  local -r NVIDIA_CONF="$(
+  local -r nvidia_conf="$(
     cat << EOF
 title    Arch Linux
-linux    /${VMLINUZ}
-initrd   /${UCODE}
-initrd   /${INITRAMFS}
-options  root=PARTUUID=${ROOT_PARTUUID} ${NVIDIA_PARAMS} loglevel=3
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs}
+options  root=PARTUUID=${root_partuuid} ${nvidia_params} loglevel=3
 EOF
   )"
 
-  local -r NVIDIA_FALLBACK_CONF="$(
+  local -r nvidia_fallback_conf="$(
     cat << EOF
 title    Arch Linux (fallback initramfs)
-linux    /${VMLINUZ}
-initrd   /${UCODE}
-initrd   /${INITRAMFS_FALLBACK}
-options  root=PARTUUID=${ROOT_PARTUUID} ${NVIDIA_PARAMS} debug
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs_fallback}
+options  root=PARTUUID=${root_partuuid} ${nvidia_params} debug
 EOF
   )"
 
-  local -r AMD_CONF="$(
+  local -r amd_conf="$(
     cat << EOF
 title    Arch Linux
-linux    /${VMLINUZ}
-initrd   /${UCODE}
-initrd   /${INITRAMFS}
-options  root=PARTUUID=${ROOT_PARTUUID} ${AMD_PARAMS} loglevel=3
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs}
+options  root=PARTUUID=${root_partuuid} ${amd_params} loglevel=3
 EOF
   )"
 
-  local -r AMD_FALLBACK_CONF="$(
+  local -r amd_fallback_conf="$(
     cat << EOF
 title    Arch Linux (fallback initramfs)
-linux    /${VMLINUZ}
-initrd   /${UCODE}
-initrd   /${INITRAMFS_FALLBACK}
-options  root=PARTUUID=${ROOT_PARTUUID} ${AMD_PARAMS} debug
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs_fallback}
+options  root=PARTUUID=${root_partuuid} ${amd_params} debug
 EOF
   )"
 
-  echo "${LOADER_CONF}" > /mnt/boot/loader/loader.conf
+  echo "${loader_conf}" > /mnt/boot/loader/loader.conf
 
   case "${GPU}" in
     'nvidia')
-      echo "${NVIDIA_CONF}" > "${ENTRIES}/arch.conf"
-      echo "${NVIDIA_FALLBACK_CONF}" > "${ENTRIES}/arch_fallback.conf"
+      echo "${nvidia_conf}" > "${entries}/arch.conf"
+      echo "${nvidia_fallback_conf}" > "${entries}/arch_fallback.conf"
       ;;
     'amd')
-      echo "${AMD_CONF}" > "${ENTRIES}/arch.conf"
-      echo "${AMD_FALLBACK_CONF}" > "${ENTRIES}/arch_fallback.conf"
+      echo "${amd_conf}" > "${entries}/arch.conf"
+      echo "${amd_fallback_conf}" > "${entries}/arch_fallback.conf"
       ;;
   esac
 }
 
 enable_services() {
-  to-arch systemctl enable {iptables,docker,systemd-boot-update}.service {fstrim,reflector}.timer
+  to_arch systemctl enable {iptables,docker,systemd-boot-update}.service {fstrim,reflector}.timer
 
   case "${DE}" in
     'i3')
-      to-arch systemctl enable systemd-{networkd,resolved}.service
+      to_arch systemctl enable systemd-{networkd,resolved}.service
       ;;
     'xfce')
-      to-arch systemctl enable {lightdm,systemd-{networkd,resolved}}.service
+      to_arch systemctl enable {lightdm,systemd-{networkd,resolved}}.service
       ;;
     'gnome')
-      to-arch systemctl enable {gdm,NetworkManager}.service
+      to_arch systemctl enable {gdm,NetworkManager}.service
       ;;
     'kde')
-      to-arch systemctl enable {sddm,NetworkManager}.service
+      to_arch systemctl enable {sddm,NetworkManager}.service
       ;;
   esac
 }
--- minimal_install.sh
+++ minimal_install.sh
@@ -4,11 +4,11 @@ set -eu
 
 unalias -a
 
-to-arch() { arch-chroot /mnt "${@}"; }
+to_arch() { arch-chroot /mnt "${@}"; }
 
 readonly KERNEL='linux-zen'
 readonly DISK='/dev/sda'
-CPU_INFO="$(grep 'model name' /proc/cpuinfo | awk --field-separator='[ (]' 'NR==1 {print $3}')" && readonly CPU_INFO
+readonly CPU_INFO && CPU_INFO="$(grep 'model name' /proc/cpuinfo | awk --field-separator='[ (]' 'NR==1 {print $3}')"
 
 packagelist="base \
   base-devel \
@@ -40,22 +40,20 @@ time_setting() {
 }
 
 partitioning() {
-  local -r EFI_PART_TYPE="$(sgdisk --list-types \
+  local -r efi_part_type="$(sgdisk --list-types \
     | grep 'ef00' \
     | awk '{print $6,$7,$8}')"
-  local -r NORMAL_PART_TYPE="$(sgdisk --list-types \
+  local -r normal_part_type="$(sgdisk --list-types \
     | grep '8300' \
     | awk '{print $2,$3}')"
 
   sgdisk --zap-all "${DISK}"
-  sgdisk --new='0::+512M' -typecode='0:ef00' --change-name="0:${EFI_PART_TYPE}" "${DISK}"
-  sgdisk --new='0::' --typecode='0:8300' --change-name="0:${NORMAL_PART_TYPE}" "${DISK}"
+  sgdisk --new='0::+512M' -typecode='0:ef00' --change-name="0:${efi_part_type}" "${DISK}"
+  sgdisk --new='0::' --typecode='0:8300' --change-name="0:${normal_part_type}" "${DISK}"
 
-  # format
   mkfs.fat -F 32 "${DISK}1"
   mkfs.ext4 "${DISK}2"
 
-  # mount
   mount "${DISK}2" /mnt
   mount --mkdir --options='fmask=0077,dmask=0077' "${DISK}1" /mnt/boot
 }
@@ -69,88 +67,88 @@ installation() {
 }
 
 configuration() {
-  to-arch reflector --country='Japan' --age=24 --protocol='https' --sort='rate' --save='/etc/pacman.d/mirrorlist'
-  to-arch ln --symbolic --force /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
-  to-arch hwclock --systohc --utc
-  to-arch sed --in-place \
+  to_arch reflector --country='Japan' --age=24 --protocol='https' --sort='rate' --save='/etc/pacman.d/mirrorlist'
+  to_arch ln --symbolic --force /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+  to_arch hwclock --systohc --utc
+  to_arch sed --in-place \
     --expression='s/^#\(en_US.UTF-8 UTF-8\)/\1/' \
     --expression='s/^#\(ja_JP.UTF-8 UTF-8\)/\1/' /etc/locale.gen
-  to-arch sed --in-place --expression='s/^#\(ParallelDownloads\)/\1/' /etc/pacman.conf
-  to-arch locale-gen
-  to-arch sed --expression='s/^# \(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers | EDITOR='/usr/bin/tee' to-arch visudo &> /dev/null
+  to_arch sed --in-place --expression='s/^#\(ParallelDownloads\)/\1/' /etc/pacman.conf
+  to_arch locale-gen
+  to_arch sed --expression='s/^# \(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers | EDITOR='/usr/bin/tee' to_arch visudo &> /dev/null
   echo 'LANG=en_US.UTF-8' > /mnt/etc/locale.conf
   echo 'KEYMAP=us' >> /mnt/etc/vconsole.conf
   echo 'virtualbox' > /mnt/etc/hostname
 }
 
 networking() {
-  local -r NET_INTERFACE="$(ip -br link show | awk 'NR==2 {print $1}')"
+  local -r net_interface="$(ip -br link show | awk 'NR==2 {print $1}')"
 
-  local -r HOSTS="$(
+  local -r hosts="$(
     cat << EOF
 127.0.0.1       localhost
 ::1             localhost
 EOF
   )"
 
-  local -r WIRED="[Match]
-Name=${NET_INTERFACE}
+  local -r wired="[Match]
+Name=${net_interface}
 
 [Network]
 DHCP=yes"
 
-  local -r RESOLVED="[Resolve]
+  local -r resolved="[Resolve]
 DNS=2001:4860:4860::8888 2001:4860:4860::8844"
 
   mkdir /etc/systemd/resolved.conf.d
   ln --symbolic --force /run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
 
-  echo "${HOSTS}" >> /mnt/etc/hosts
-  echo "${WIRED}" > /mnt/etc/systemd/network/20-wired.network
-  echo "${RESOLVED}" > /etc/systemd/resolved.conf.d/dns_servers.conf
+  echo "${hosts}" >> /mnt/etc/hosts
+  echo "${wired}" > /mnt/etc/systemd/network/20-wired.network
+  echo "${resolved}" > /etc/systemd/resolved.conf.d/dns_servers.conf
 }
 
 create_user() {
-  local -r USER_NAME='virt'
+  local -r user_name='virt'
 
-  echo 'root:root' | to-arch chpasswd
-  to-arch useradd --create-home --groups='wheel' --shell='/bin/bash' "${USER_NAME}"
-  echo "${USER_NAME}:virt" | to-arch chpasswd
+  echo 'root:root' | to_arch chpasswd
+  to_arch useradd --create-home --groups='wheel' --shell='/bin/bash' "${user_name}"
+  echo "${user_name}:virt" | to_arch chpasswd
 }
 
 replacement() {
-  to-arch sed --in-place \
+  to_arch sed --in-place \
     --expression='s/^#\(NTP=\)/\1ntp.nict.jp/' \
     --expression='s/^#\(FallbackNTP=\).*/\1ntp1.jst.mfeed.ad.jp ntp2.jst.mfeed.ad.jp ntp3.jst.mfeed.ad.jp/' /etc/systemd/timesyncd.conf
   # shellcheck disable=SC2016
-  to-arch sed --in-place \
+  to_arch sed --in-place \
     --expression='s/\(-march=\)x86-64 -mtune=generic/\1skylake/' \
     --expression='s/^#\(MAKEFLAGS=\).*/\1"-j$(($(nproc)+1))"/' \
     --expression='s/^#\(BUILDDIR\)/\1/' /etc/makepkg.conf
-  to-arch sed --in-place \
+  to_arch sed --in-place \
     --expression='s/^# \(--country\) France,Germany/\1 Japan/' \
     --expression='s/^--latest 5/# &/' \
     --expression='s/^\(--sort\) age/\1 rate/' /etc/xdg/reflector/reflector.conf
-  to-arch sed --in-place --expression='s/^#\(Color\)/\1/' /etc/pacman.conf
+  to_arch sed --in-place --expression='s/^#\(Color\)/\1/' /etc/pacman.conf
   echo -e '\n--age 24' >> /mnt/etc/xdg/reflector/reflector.conf
 
-  to-arch pacman --sync --refresh --refresh
+  to_arch pacman --sync --refresh --refresh
 }
 
 boot_loader() {
   find_boot() { find /mnt/boot -type 'f' -name "${1}"; }
 
-  to-arch bootctl install
+  to_arch bootctl install
 
-  local -r ROOT_PARTUUID="$(blkid --match-tag='PARTUUID' --output='value' "${DISK}2")"
-  local -r VMLINUZ="$(find_boot "*vmlinuz*${KERNEL}*" | awk --field-separator='/' '{print $4}')"
-  local -r UCODE="$(find_boot '*ucode*' | awk --field-separator='/' '{print $4}')"
-  local -r INITRAMFS="$(find_boot "*initramfs*${KERNEL}*" | awk --field-separator='/' 'NR==1 {print $4}')"
-  local -r INITRAMFS_FALLBACK="$(find_boot "*initramfs*${KERNEL}*" | awk --field-separator='/' 'END {print $4}')"
-  local -r KERNEL_PARAMS='rw panic=180'
-  local -r ENTRIES='/mnt/boot/loader/entries'
+  local -r root_partuuid="$(blkid --match-tag='PARTUUID' --output='value' "${DISK}2")"
+  local -r vmlinuz="$(find_boot "*vmlinuz*${KERNEL}*" | awk --field-separator='/' '{print $4}')"
+  local -r ucode="$(find_boot '*ucode*' | awk --field-separator='/' '{print $4}')"
+  local -r initramfs="$(find_boot "*initramfs*${KERNEL}*" | awk --field-separator='/' 'NR==1 {print $4}')"
+  local -r initramfs_fallback="$(find_boot "*initramfs*${KERNEL}*" | awk --field-separator='/' 'END {print $4}')"
+  local -r kernel_params='rw panic=180'
+  local -r entries='/mnt/boot/loader/entries'
 
-  local -r LOADER_CONF="$(
+  local -r loader_conf="$(
     cat << EOF
 timeout      15
 console-mode max
@@ -158,33 +156,33 @@ editor       no
 EOF
   )"
 
-  local -r ENTRIES_CONF="$(
+  local -r entries_conf="$(
     cat << EOF
 title    Arch Linux
-linux    /${VMLINUZ}
-initrd   /${UCODE}
-initrd   /${INITRAMFS}
-options  root=PARTUUID=${ROOT_PARTUUID} ${KERNEL_PARAMS} loglevel=3
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs}
+options  root=PARTUUID=${root_partuuid} ${kernel_params} loglevel=3
 EOF
   )"
 
-  local -r ENTRIES_CONF_FALLBACK="$(
+  local -r entries_conf_fallback="$(
     cat << EOF
 title    Arch Linux (fallback initramfs)
-linux    /${VMLINUZ}
-initrd   /${UCODE}
-initrd   /${INITRAMFS_FALLBACK}
-options  root=PARTUUID=${ROOT_PARTUUID} ${KERNEL_PARAMS} debug
+linux    /${vmlinuz}
+initrd   /${ucode}
+initrd   /${initramfs_fallback}
+options  root=PARTUUID=${root_partuuid} ${kernel_params} debug
 EOF
   )"
 
-  echo "${LOADER_CONF}" > /mnt/boot/loader/loader.conf
-  echo "${ENTRIES_CONF}" > "${ENTRIES}/arch.conf"
-  echo "${ENTRIES_CONF_FALLBACK}" > "${ENTRIES}/arch_fallback.conf"
+  echo "${loader_conf}" > /mnt/boot/loader/loader.conf
+  echo "${entries_conf}" > "${entries}/arch.conf"
+  echo "${entries_conf_fallback}" > "${entries}/arch_fallback.conf"
 }
 
 enable_services() {
-  to-arch systemctl enable systemd-{boot-update,networkd,resolved}.service reflector.timer
+  to_arch systemctl enable systemd-{boot-update,networkd,resolved}.service reflector.timer
 }
 
 main() {
