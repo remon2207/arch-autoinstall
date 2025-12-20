6ae8b8a9aa9674fed7d74a67b043a17914f276b0
remon2207 <remon2207.develop@gmail.com>
2025-12-20 20:33:29 +0900

    Add kde for minimal_install

---
+++ minimal_install_kde.sh
@@ -0,0 +1,188 @@
+#!/usr/bin/env bash
+
+set -eu
+
+unalias -a
+
+to_arch() { arch-chroot /mnt "${@}"; }
+
+readonly KERNEL='linux-zen'
+readonly DISK='/dev/sda'
+CPU_INFO="$(grep 'model name' /proc/cpuinfo | awk --field-separator='[ (]' 'NR==1 {print $3}')"
+readonly CPU_INFO
+
+packagelist="base \
+    base-devel \
+    ${KERNEL} \
+    ${KERNEL}-headers \
+    linux-firmware \
+    vi \
+    vim \
+    sudo \
+    curl \
+    man-db \
+    reflector \
+    plasma-meta \
+    dolphin \
+    gwenview \
+    spectacle \
+    konsole \
+    kwalletmanager \
+    ufw \
+    noto-fonts \
+    noto-fonts-cjk \
+    noto-fonts-emoji \
+    noto-fonts-extra \
+    pipewire \
+    pipewire-alsa \
+    pipewire-jack \
+    pipewire-pulse \
+    wireplumber \
+    fcitx5 \
+    fcitx5-configtool \
+    fcitx5-gtk \
+    fcitx5-mozc \
+    fcitx5-qt"
+
+selection_arguments() {
+    case "${CPU_INFO}" in
+        'Intel')
+            packagelist="${packagelist} intel-ucode"
+            ;;
+        'AMD')
+            packagelist="${packagelist} amd-ucode"
+            ;;
+    esac
+}
+
+time_setting() {
+    hwclock --systohc --utc
+    timedatectl set-ntp true
+}
+
+partitioning() {
+    local -r efi_part_type="$(sgdisk --list-types \
+        | grep 'ef00' \
+        | awk '{print $6,$7,$8}')"
+    local -r normal_part_type="$(sgdisk --list-types \
+        | grep '8300' \
+        | awk '{print $2,$3}')"
+
+    sgdisk --zap-all "${DISK}"
+    sgdisk --new='0::+1G' --typecode='0:ef00' --change-name="0:${efi_part_type}" "${DISK}"
+    sgdisk --new='0::' --typecode='0:8300' --change-name="0:${normal_part_type}" "${DISK}"
+
+    mkfs.fat -F 32 "${DISK}1"
+    mkfs.ext4 "${DISK}2"
+
+    mount "${DISK}2" /mnt
+    mount --mkdir "${DISK}1" /mnt/boot
+}
+
+installation() {
+    # reflector --country='Japan' --age=24 --protocol='https,http' --sort='rate' --save='/etc/pacman.d/mirrorlist'
+    # pacman -Syy
+    # sed --in-place --expression='s/^#\(ParallelDownloads\)/\1/' /etc/pacman.conf
+    # shellcheck disable=2086
+    pacstrap -K /mnt ${packagelist}
+
+    to_arch sed -i \
+        -e "s/^\(PRESETS=('default')\)/#\1/" \
+        -e "s/^#\(PRESETS=('default' 'fallback')\)/\1/" \
+        -e "s/^#\(fallback_image.*\)/\1/" \
+        -e "s/^#\(fallback_options.*\)/\1/" "/etc/mkinitcpio.d/${KERNEL}.preset"
+
+    genfstab -t 'PARTUUID' /mnt >> /mnt/etc/fstab
+    to_arch sed -i "s/\(fmask\)=0022\(,dmask\)=0022/\1=0077\2=0077/" /etc/fstab
+}
+
+configuration() {
+    # to_arch reflector --country='Japan' --age=24 --protocol='https' --sort='rate' --save='/etc/pacman.d/mirrorlist'
+    to_arch ln --symbolic --force /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+    to_arch hwclock --systohc --utc
+    to_arch sed --in-place --expression='s/^#\(en_US.UTF-8 UTF-8\)/\1/' /etc/locale.gen
+    # to_arch sed --in-place --expression='s/^#\(ParallelDownloads\)/\1/' /etc/pacman.conf
+    to_arch locale-gen
+    echo 'LANG=en_US.UTF-8' > /mnt/etc/locale.conf
+    echo 'KEYMAP=us' >> /mnt/etc/vconsole.conf
+    echo 'virtualbox' > /mnt/etc/hostname
+    to_arch mkinitcpio -P
+    to_arch sed --expression='s/^# \(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers | EDITOR='tee' to_arch visudo &> /dev/null
+}
+
+create_user() {
+    local -r user_name='virt'
+
+    echo 'root:root' | to_arch chpasswd
+    to_arch useradd --create-home --groups='wheel' --shell='/bin/bash' "${user_name}"
+    echo "${user_name}:virt" | to_arch chpasswd
+}
+
+replacement() {
+    to_arch cp /etc/systemd/timesyncd.conf{,.org}
+    to_arch sed --in-place \
+        --expression='s/^#\(NTP=\)/\1ntp.nict.jp/' \
+        --expression='s/^#\(FallbackNTP=\).*/\1ntp1.v6.mfeed.ad.jp ntp2.v6.mfeed.ad.jp ntp3.v6.mfeed.ad.jp/' /etc/systemd/timesyncd.conf
+    to_arch sed --in-place \
+        --expression='s/^# \(--country\) France,Germany/\1 Japan/' \
+        --expression='s/^--latest 5/# &/' \
+        --expression='s/^\(--sort\) age/\1 rate/' \
+        --expression='s/^\(--protocol https\)/\1,http/' /etc/xdg/reflector/reflector.conf
+    to_arch sed --in-place --expression='s/^#\(Color\)/\1/' /etc/pacman.conf
+    echo -e '\n--age 24' >> /mnt/etc/xdg/reflector/reflector.conf
+
+    to_arch pacman --sync --refresh --refresh
+}
+
+boot_loader() {
+    find_boot() { find /mnt/boot -type 'f' -name "${1}"; }
+
+    to_arch bootctl install
+
+    local -r root_partuuid="$(blkid --match-tag='PARTUUID' --output='value' "${DISK}2")"
+    local -r vmlinuz="$(find_boot "*vmlinuz*${KERNEL}*" | awk --field-separator='/' '{print $4}')"
+    local -r ucode="$(find_boot '*ucode*' | awk --field-separator='/' '{print $4}')"
+    local -r initramfs="$(find_boot "*initramfs*${KERNEL}*" | awk --field-separator='/' 'NR==1 {print $4}')"
+    local -r initramfs_fallback="$(find_boot "*initramfs*${KERNEL}*" | awk --field-separator='/' 'END {print $4}')"
+    local -r kernel_params='rw panic=180'
+    local -r entries='/mnt/boot/loader/entries'
+
+    local -r loader_conf='timeout 15
+console-mode max
+editor no'
+
+    local -r entries_conf="title Arch Linux
+linux /${vmlinuz}
+initrd /${ucode}
+initrd /${initramfs}
+options root=PARTUUID=${root_partuuid} ${kernel_params} loglevel=3"
+
+    local -r entries_conf_fallback="title Arch Linux (fallback initramfs)
+linux /${vmlinuz}
+initrd /${ucode}
+initrd /${initramfs_fallback}
+options root=PARTUUID=${root_partuuid} ${kernel_params} debug"
+
+    echo "${loader_conf}" > /mnt/boot/loader/loader.conf
+    echo "${entries_conf}" > "${entries}/arch.conf"
+    echo "${entries_conf_fallback}" > "${entries}/arch_fallback.conf"
+}
+
+enable_services() {
+    to_arch systemctl enable systemd-{boot-update,sddm,NetworkManager}.service reflector.timer
+}
+
+main() {
+    selection_arguments
+    time_setting
+    partitioning
+    installation
+    configuration
+    networking
+    create_user
+    replacement
+    boot_loader
+    enable_services
+}
+
+main "${@}"
